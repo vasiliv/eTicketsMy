e16c3fc5d09efc1c458a92a9882350d69c5fb9a7
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-11 19:13:53 +0400

    3-10 finished

--- eTicketsMy/Data/AppDbContext.cs
+++ eTicketsMy/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
-﻿using eTicketsMy.Models;
+﻿using eTicketsMy.Data.Enums;
+using eTicketsMy.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace eTicketsMy.Data
 {
@@ -16,6 +18,7 @@ namespace eTicketsMy.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Many-to-many relationship fluentAPI
             modelBuilder.Entity<Actor_Movie>()
                 .HasKey(am => new { am.ActorId, am.MovieId });
             modelBuilder.Entity<Actor_Movie>()
@@ -26,6 +29,284 @@ namespace eTicketsMy.Data
                 .HasOne(am => am.Movie)
                 .WithMany(m => m.Actors_Movies)
                 .HasForeignKey(am => am.MovieId);
+
+            base.OnModelCreating(modelBuilder);
+            //Seed Actors
+            modelBuilder.Entity<Actor>().HasData(new Actor {
+                Id= 1,
+                FullName = "Actor 1",
+                Bio = "This is the Bio of the first actor",
+                ProfilePictureURL = "http://dotnethow.net/images/actors/actor-1.jpeg"
+            });
+            modelBuilder.Entity<Actor>().HasData(new Actor
+            {
+                Id = 2,
+                FullName = "Actor 2",
+                Bio = "This is the Bio of the second actor",
+                ProfilePictureURL = "http://dotnethow.net/images/actors/actor-2.jpeg"
+            });
+            modelBuilder.Entity<Actor>().HasData(new Actor
+            {
+                Id = 3,
+                FullName = "Actor 3",
+                Bio = "This is the Bio of the third actor",
+                ProfilePictureURL = "http://dotnethow.net/images/actors/actor-3.jpeg"
+
+            });
+            modelBuilder.Entity<Actor>().HasData(new Actor
+            {
+                Id = 4,
+                FullName = "Actor 4",
+                Bio = "This is the Bio of the fourth actor",
+                ProfilePictureURL = "http://dotnethow.net/images/actors/actor-4.jpeg"
+            });
+            modelBuilder.Entity<Actor>().HasData(new Actor
+            {
+                Id = 5,
+                FullName = "Actor 5",
+                Bio = "This is the Bio of the fifth actor",
+                ProfilePictureURL = "http://dotnethow.net/images/actors/actor-5.jpeg"
+            });
+
+            //Seed Cinemas
+            modelBuilder.Entity<Cinema>().HasData(new Cinema
+            {
+                Id = 1,
+                Name = "Cinema 1",
+                Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                Description = "This is the description of the first cinema"
+            });
+            modelBuilder.Entity<Cinema>().HasData(new Cinema
+            {
+                Id = 2,
+                Name = "Cinema 2",
+                Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                Description = "This is the description of the second cinema"
+            });
+            modelBuilder.Entity<Cinema>().HasData(new Cinema
+            {
+                Id = 3,
+                Name = "Cinema 3",
+                Logo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                Description = "This is the description of the third cinema"
+            });
+            modelBuilder.Entity<Cinema>().HasData(new Cinema
+            {
+                Id = 4,
+                Name = "Cinema 4",
+                Logo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                Description = "This is the description of the fourth cinema"
+            });
+            modelBuilder.Entity<Cinema>().HasData(new Cinema
+            {
+                Id = 5,
+                Name = "Cinema 5",
+                Logo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                Description = "This is the description of the fifth cinema"
+            });
+
+            //Seed Movies
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 1,
+                Name = "Life",
+                Description = "This is the Life movie description",
+                Price = 39.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-3.jpeg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(10),
+                CinemaId = 3,
+                ProducerId = 3,
+                MovieCategory = MovieCategory.Documentary
+            });
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 2,
+                Name = "The Shawshank Redemption",
+                Description = "This is the Shawshank Redemption description",
+                Price = 29.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-1.jpeg",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(3),
+                CinemaId = 1,
+                ProducerId = 1,
+                MovieCategory = MovieCategory.Action
+            });
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 3,
+                Name = "Race",
+                Description = "This is the Race movie description",
+                Price = 39.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-6.jpeg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(-5),
+                CinemaId = 1,
+                ProducerId = 2,
+                MovieCategory = MovieCategory.Documentary
+            });
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 4,
+                Name = "Ghost",
+                Description = "This is the Ghost movie description",
+                Price = 39.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-4.jpeg",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(7),
+                CinemaId = 4,
+                ProducerId = 4,
+                MovieCategory = MovieCategory.Horror
+            });
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 5,
+                Name = "Scoob",
+                Description = "This is the Scoob movie description",
+                Price = 39.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-7.jpeg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(-2),
+                CinemaId = 1,
+                ProducerId = 3,
+                MovieCategory = MovieCategory.Cartoon
+            });
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 6,
+                Name = "Cold Soles",
+                Description = "This is the Cold Soles movie description",
+                Price = 39.50,
+                ImageURL = "http://dotnethow.net/images/movies/movie-8.jpeg",
+                StartDate = DateTime.Now.AddDays(3),
+                EndDate = DateTime.Now.AddDays(20),
+                CinemaId = 1,
+                ProducerId = 5,
+                MovieCategory = MovieCategory.Drama
+            });
+            //Seed Actors_Movies
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 1,
+                MovieId = 1
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 3,
+                MovieId = 1
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 1,
+                MovieId = 2
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 4,
+                MovieId = 2
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 1,
+                MovieId = 3
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 2,
+                MovieId = 3
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 5,
+                MovieId = 3
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 2,
+                MovieId = 4
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 3,
+                MovieId = 4
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 4,
+                MovieId = 4
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 2,
+                MovieId = 5
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 3,
+                MovieId = 5
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 4,
+                MovieId = 5
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 5,
+                MovieId = 5
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 3,
+                MovieId = 6
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 4,
+                MovieId = 6
+            });
+            modelBuilder.Entity<Actor_Movie>().HasData(new Actor_Movie
+            {
+                ActorId = 5,
+                MovieId = 6
+            });
+
+            //Seed Producers
+            modelBuilder.Entity<Producer>().HasData(new Producer
+            {
+                Id = 1,
+                FullName = "Producer 1",
+                Bio = "This is the Bio of the first producer",
+                ProfilePictureURL = "http://dotnethow.net/images/producers/producer-1.jpeg"
+            });
+            modelBuilder.Entity<Producer>().HasData(new Producer
+            {
+                Id = 2,
+                FullName = "Producer 2",
+                Bio = "This is the Bio of the second producer",
+                ProfilePictureURL = "http://dotnethow.net/images/producers/producer-2.jpeg"
+            });
+            modelBuilder.Entity<Producer>().HasData(new Producer
+            {
+                Id = 3,
+                FullName = "Producer 3",
+                Bio = "This is the Bio of the third producer",
+                ProfilePictureURL = "http://dotnethow.net/images/producers/producer-3.jpeg"
+            });
+            modelBuilder.Entity<Producer>().HasData(new Producer
+            {
+                Id = 4,
+                FullName = "Producer 4",
+                Bio = "This is the Bio of the fourth producer",
+                ProfilePictureURL = "http://dotnethow.net/images/producers/producer-4.jpeg"
+            });
+            modelBuilder.Entity<Producer>().HasData(new Producer
+            {
+                Id = 5,
+                FullName = "Producer 5",
+                Bio = "This is the Bio of the fifth producer",
+                ProfilePictureURL = "http://dotnethow.net/images/producers/producer-5.jpeg"
+            });            
         }
     }
 }
---
+++ eTicketsMy/Migrations/20230111150948_Seed.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTicketsMy.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "Bio", "FullName", "ProfilePictureURL" },
+                values: new object[,]
+                {
+                    { 1, "This is the Bio of the first actor", "Actor 1", "http://dotnethow.net/images/actors/actor-1.jpeg" },
+                    { 2, "This is the Bio of the second actor", "Actor 2", "http://dotnethow.net/images/actors/actor-2.jpeg" },
+                    { 3, "This is the Bio of the third actor", "Actor 3", "http://dotnethow.net/images/actors/actor-3.jpeg" },
+                    { 4, "This is the Bio of the fourth actor", "Actor 4", "http://dotnethow.net/images/actors/actor-4.jpeg" },
+                    { 5, "This is the Bio of the fifth actor", "Actor 5", "http://dotnethow.net/images/actors/actor-5.jpeg" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cinemas",
+                columns: new[] { "Id", "Description", "Logo", "Name" },
+                values: new object[,]
+                {
+                    { 1, "This is the description of the first cinema", "http://dotnethow.net/images/cinemas/cinema-1.jpeg", "Cinema 1" },
+                    { 2, "This is the description of the second cinema", "http://dotnethow.net/images/cinemas/cinema-2.jpeg", "Cinema 2" },
+                    { 3, "This is the description of the third cinema", "http://dotnethow.net/images/cinemas/cinema-3.jpeg", "Cinema 3" },
+                    { 4, "This is the description of the fourth cinema", "http://dotnethow.net/images/cinemas/cinema-4.jpeg", "Cinema 4" },
+                    { 5, "This is the description of the fifth cinema", "http://dotnethow.net/images/cinemas/cinema-5.jpeg", "Cinema 5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Producers",
+                columns: new[] { "Id", "Bio", "FullName", "ProfilePictureURL" },
+                values: new object[,]
+                {
+                    { 1, "This is the Bio of the first producer", "Producer 1", "http://dotnethow.net/images/producers/producer-1.jpeg" },
+                    { 2, "This is the Bio of the second producer", "Producer 2", "http://dotnethow.net/images/producers/producer-2.jpeg" },
+                    { 3, "This is the Bio of the third producer", "Producer 3", "http://dotnethow.net/images/producers/producer-3.jpeg" },
+                    { 4, "This is the Bio of the fourth producer", "Producer 4", "http://dotnethow.net/images/producers/producer-4.jpeg" },
+                    { 5, "This is the Bio of the fifth producer", "Producer 5", "http://dotnethow.net/images/producers/producer-5.jpeg" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "CinemaId", "Description", "EndDate", "ImageURL", "MovieCategory", "Name", "Price", "ProducerId", "StartDate" },
+                values: new object[,]
+                {
+                    { 2, 1, "This is the Shawshank Redemption description", new DateTime(2023, 1, 14, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7843), "http://dotnethow.net/images/movies/movie-1.jpeg", 1, "The Shawshank Redemption", 29.5, 1, new DateTime(2023, 1, 11, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7837) },
+                    { 3, 1, "This is the Race movie description", new DateTime(2023, 1, 6, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7873), "http://dotnethow.net/images/movies/movie-6.jpeg", 4, "Race", 39.5, 2, new DateTime(2023, 1, 1, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7871) },
+                    { 1, 3, "This is the Life movie description", new DateTime(2023, 1, 21, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(6447), "http://dotnethow.net/images/movies/movie-3.jpeg", 4, "Life", 39.5, 3, new DateTime(2023, 1, 1, 19, 9, 48, 375, DateTimeKind.Local).AddTicks(3410) },
+                    { 5, 1, "This is the Scoob movie description", new DateTime(2023, 1, 9, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7925), "http://dotnethow.net/images/movies/movie-7.jpeg", 5, "Scoob", 39.5, 3, new DateTime(2023, 1, 1, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7923) },
+                    { 4, 4, "This is the Ghost movie description", new DateTime(2023, 1, 18, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7900), "http://dotnethow.net/images/movies/movie-4.jpeg", 6, "Ghost", 39.5, 4, new DateTime(2023, 1, 11, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7899) },
+                    { 6, 1, "This is the Cold Soles movie description", new DateTime(2023, 1, 31, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7953), "http://dotnethow.net/images/movies/movie-8.jpeg", 3, "Cold Soles", 39.5, 5, new DateTime(2023, 1, 14, 19, 9, 48, 376, DateTimeKind.Local).AddTicks(7951) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors_Movies",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[,]
+                {
+                    { 1, 2 },
+                    { 3, 6 },
+                    { 4, 4 },
+                    { 3, 4 },
+                    { 2, 4 },
+                    { 5, 5 },
+                    { 4, 5 },
+                    { 4, 6 },
+                    { 3, 5 },
+                    { 3, 1 },
+                    { 1, 1 },
+                    { 5, 3 },
+                    { 2, 3 },
+                    { 1, 3 },
+                    { 4, 2 },
+                    { 2, 5 },
+                    { 5, 6 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 4, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 4, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 5, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors_Movies",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 5, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Producers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Producers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Producers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Producers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Producers",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
