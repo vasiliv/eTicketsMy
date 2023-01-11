0defa6f14a5b0a3c1a0bb2388d1d61966736f9c8
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-11 11:40:45 +0400

    3-6 finished

---
+++ eTicketsMy/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eTicketsMy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicketsMy.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }        
+    }
+}
