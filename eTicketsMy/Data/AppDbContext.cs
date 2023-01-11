272d792fa66c7d53e7985e5f5722ec807cdf8670
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-11 12:25:29 +0400

    3-8 finished

--- eTicketsMy/Data/AppDbContext.cs
+++ eTicketsMy/Data/AppDbContext.cs
@@ -12,6 +12,20 @@ namespace eTicketsMy.Data
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<Producer> Producers { get; set; }        
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>()
+                .HasKey(am => new { am.ActorId, am.MovieId });
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(am => am.Actor)
+                .WithMany(a => a.Actors_Movies)
+                .HasForeignKey(am => am.ActorId);
+            modelBuilder.Entity<Actor_Movie>()
+                .HasOne(am => am.Movie)
+                .WithMany(m => m.Actors_Movies)
+                .HasForeignKey(am => am.MovieId);
+        }
     }
 }
